4aa638184c1689be81138ea374a15e4ee4daa4e3
luca <luca.carnaroli98@gmail.com>
2020-01-17 17:08:37 +0100

    img.next

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-alert('ciao');
+// le immagini scorrono verso dx
+$(document).ready(function () {
+
+    $('.next').click(
+      function () {
+        nextImg();
+      }
+    );
+
+    $('.prev').click(
+      function () {
+        prevImg();
+      }
+    );
+  }
+);
+
+
+
+// funzioni
+function nextImg() {
+  var imgActive = $('img.active');
+  var imgNext = imgActive.next();
+  imgActive.removeClass('active');
+  imgNext.addClass('active');
+
+}
+
+function prevImg()  {
+
+}

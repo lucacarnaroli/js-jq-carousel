563e87b8595b06ab003f90ea325e0455d77117b2
luca <luca.carnaroli98@gmail.com>
2020-01-17 18:28:08 +0100

    carousel

--- main.js
+++ main.js
@@ -15,17 +15,34 @@ $(document).ready(function () {
   }
 );
 
-
-
 // funzioni
 function nextImg() {
-  var imgActive = $('img.active');
-  var imgNext = imgActive.next();
-  imgActive.removeClass('active');
-  imgNext.addClass('active');
-
+  var iconActive = $('i.active');
+  var iconNext = iconActive.next();
+  var imageActive = $('img.active');
+  var imageNext = imageActive.next();
+  if (imageActive.hasClass('last')) {
+    imageNext = $('img.first');
+    iconNext = $('i.first');
+  }
+  iconActive.removeClass('active');
+  iconNext.addClass('active');
+  imageActive.removeClass('active');
+  imageNext.addClass('active');
 }
 
 function prevImg()  {
+  var iconActive = $('i.active');
+  var iconNext = iconActive.prev();
+  var imageActive = $('img.active');
+  var imagePrev = imageActive.prev();
+  if (imageActive.hasClass('first')) {
+    imagePrev = $('img.last');
+    iconNext = $('i.last');
+  }
+  imageActive.removeClass('active');
+  imagePrev.addClass('active');
+  iconActive.removeClass('active');
+  iconNext.addClass('active');
 
 }
